f2c94cbf61445269e0ff77213636bf739d922f80
Julian Lubenov <julian.lubenov@plana.solutions>
2026-03-24 09:50:51 +0200

    Fixed ow_invoice_iban_qr for odoo 16

--- ow_invoice_iban_qr/models/res_company.py
+++ ow_invoice_iban_qr/models/res_company.py
@@ -8,4 +8,4 @@ class ResCompany(models.Model):
 
     _inherit = 'res.company'
 
-    iban_qr_number = fields.Many2one('res.partner.bank', domain="[('partner_id','=', partner_id)]")
+    iban_qr_number = fields.Many2one('res.partner.bank')
